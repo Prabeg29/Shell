ab5915b89aad5dbcdabaa305f7d2ccaf66aafda5
Prabeg Shakya <shakyaprabeg@gmail.com>
2020-11-26 09:20:19 +0545

    modified the read_line function

--- src/main.c
+++ src/main.c
@@ -1,46 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFSIZE 255
+#define TOKEN_DELIMITER " \t\r\n\a"
 
-
-char *read_line()
+/*
+ * Function:  split_line
+ * -----------------------------
+ *  splits a line into tokens using strtok()
+ *
+ * line: a line read from terminal
+ *
+ * returns: an array of pointers to individual tokens
+ */
+/* char **split_line(char *line)
 {
 	int position = 0;
-	char c, *buffer = malloc(sizeof(char));
-	
-	if(buffer == NULL)
-	{
-    	printf("Could not allocate memory!");
-    	exit(1);
-  	}
-  
-  	// Read input until a new line is hit.
-	while((c = getchar()) != '\n')
-	{
-		// Reallocate buffer if it exceeds
-		buffer = realloc(buffer, (sizeof(char)));
+	char *token;
+	// pointer to an array which holds pointer(address) to the first character of the token
+	char **tokens = malloc(sizeof(char*) * 1);
 
-		if(buffer == NULL)
+	if(!tokens)
+	{
+		fprintf(stderr, "allocation error.\n");
+		exit(1);
+	}
+
+	token = strtok(line, TOKEN_DELIMITER);
+	while(token != NULL)
+	{
+		tokens[position++] = token;
+		tokens = realloc(tokens, sizeof(char*) * 1);
+
+		if(tokens == NULL)
 		{
-			printf("Could not allocate memory!");
+			fprintf(stderr, "allocation error.\n");
 			exit(1);
 		}
+		token = strtok(NULL, TOKEN_DELIMITER);
+	}
+	tokens[position] = NULL;
+	return tokens;
+} */
 
+/*
+ * Function:  read_line
+ * ----------------------------
+ * reads a line from terminal
+ *
+ * returns: a line read from terminal
+ */
+char *read_line()
+{
+	int position = 0, c, buffer_size = 10;
+	char *buffer = malloc(sizeof(char) * buffer_size);
+
+	if(buffer == NULL)
+	{
+		fprintf(stderr, "Allocation Error.\n");
+	}
+
+	while(EOF != (c=getch()) && c != '\n')
+	{
 		buffer[position++] = c;
+		if(position == buffer_size)
+		{
+			buffer_size += position;
+			buffer = realloc(buffer, sizeof(char) * buffer_size);
+			if(buffer == NULL)
+			{
+				fprintf(stderr, "Allocation Error.\n");
+			}
+		}
 	}
 	buffer[position] = '\0';
-
 	return buffer;
 }
 
+
 void shell_loop()
 {
-	char *line;
+	char *line; // **arguments;
 	do
 	{
 		printf(">>");
 		line = read_line();
+		//arguments = split_line(line);
 	} while (1);
 	
 }
